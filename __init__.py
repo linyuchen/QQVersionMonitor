a17d70d0c40b9af3dd375ddefb58f55378cae65e
linyuchen <lin.yu.chen@hotmail.com>
2025-09-28 19:20:02 +0800

    fix: update import statements to use relative paths in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from src.check_qq_update import QQNTVersionMonitor
-from src.qq_archs import QQArchExt
-from src.qq_download_link import QQDownloadLink, QQDownloadLinkManager
+from .src.check_qq_update import QQNTVersionMonitor
+from .src.qq_archs import QQArchExt
+from .src.qq_download_link import QQDownloadLink, QQDownloadLinkManager
